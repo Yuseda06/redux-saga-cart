0b3ac7736dd7cce160ca7c92598f9fea0ced3b3a
Yusri Saad <muhd.yusri@rhbgroup.com>
2024-08-15 17:09:41 +0800

    initial

--- src/getStore.js
+++ src/getStore.js
@@ -1,37 +1,35 @@
-import {
-    createStore,
-    applyMiddleware,
-    compose
-} from 'redux';
+import { createStore, applyMiddleware, compose } from "redux";
 
-import { createLogger } from 'redux-logger';
-import { Iterable } from 'immutable'
-import thunk from 'redux-thunk'
+import { createLogger } from "redux-logger";
+import { Iterable } from "immutable";
+import thunk from "redux-thunk";
 
-import { getQuery } from './utility'
-import { reducer } from './combineReducers';
-import { defaultState } from './defaultState'
+import { getQuery } from "./utility";
+import { reducer } from "./combineReducers";
+import { defaultState } from "./defaultState";
+import createSagaMiddleware from "redux-saga";
+import { initSagas } from "./initSagas";
 
 const stateTransformer = (state) => {
-    if (Iterable.isIterable(state)) return state.toJS();
-    else return state;
+  if (Iterable.isIterable(state)) return state.toJS();
+  else return state;
 };
 
 const logger = createLogger({
-    stateTransformer,
+  stateTransformer,
 });
 
-export const getStore = ()=>{
-    const middleWares = [thunk];
-    if (getQuery()['logger']) { middleWares.push(logger)}
-    const composables = [applyMiddleware(...middleWares)]
-    const enhancer = compose(
-        ... composables
-    );
-    const store = createStore(
-        reducer,
-        defaultState,
-        enhancer
-    );
-    return store;
+export const getStore = () => {
+  const middleWares = [thunk];
+  if (getQuery()["logger"]) {
+    middleWares.push(logger);
+  }
+  const composables = [applyMiddleware(...middleWares)];
+  const enhancer = compose(...composables);
+  const store = createStore(reducer, defaultState, enhancer);
+
+  console.info("Saga middleware is running");
+
+  initSagas(createSagaMiddleware);
+  return store;
 };
